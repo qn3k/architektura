7ca34492e390886ed54c9687cfcda22894a652d4
Jakub Jankowski <58094233+qn3k@users.noreply.github.com>
2024-05-26 11:47:25 +0200

    Create zaddomowe3.s

---
+++ zaddomowe3.s
@@ -0,0 +1,38 @@
+.intel_syntax noprefix
+.global main
+.text
+main:
+mov eax, [esp+8]
+mov eax, [eax+4]
+push eax
+call atoi
+add esp,4
+push eax
+call f
+add esp,4
+push eax
+mov eax, OFFSET msg
+push eax
+call printf
+add esp,8
+mov eax,0
+ret
+f:
+mov eax,[esp+4]
+cmp eax,1
+jne skok
+mov eax,1 
+ret
+skok:
+dec eax
+push eax
+call f
+add esp,4
+add eax,[esp+4]
+ret
+
+#funkcja f ma obliczac ciag fibonacciego rekurencyjnie
+
+.data
+msg:
+.asciz "Wynik = %i \n"
