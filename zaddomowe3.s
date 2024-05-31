20d48654dca3bd68b4f537f8c09679a3d1114a5d
Jakub Jankowski <58094233+qn3k@users.noreply.github.com>
2024-05-31 20:09:41 +0200

    Update zaddomowe3.s

--- zaddomowe3.s
+++ zaddomowe3.s
@@ -2,42 +2,47 @@
 .global main
 .text
 main:
-mov eax, [esp+8]
-mov eax, [eax+4]
-push eax
-call atoi
-add esp,4
-push eax
-call f
-add esp,4
-push eax
-mov eax, OFFSET msg
-push eax
-call printf
-add esp,8
-mov eax,0
-ret
-f:
-mov eax,[esp+4]
-cmp eax,1
-jne skok
-mov eax,1 
-ret
-skok:
-dec eax
-push eax
-call f
-add esp,4
-add eax,[esp+4]
-ret
+    mov eax, [esp+8]      # Pobierz pierwszy argument (adres łańcucha znaków)
+    mov eax, [eax+4]      # Pobierz drugi argument (liczba jako łańcuch znaków)
+    push eax              # Umieść na stosie
+    call atoi             # Zamień na liczbę całkowitą
+    add esp,4             # Usuń argument ze stosu
+    push eax              # Umieść liczbę na stosie
+    call f                # Wywołaj funkcję f
+    add esp,4             # Usuń argument ze stosu
+    push eax              # Umieść wynik na stosie
+    mov eax, OFFSET msg   # Pobierz adres komunikatu
+    push eax              # Umieść adres komunikatu na stosie
+    call printf           # Wywołaj funkcję printf
+    add esp,8             # Usuń dwa argumenty ze stosu
+    mov eax,0             # Zakończ program z kodem powrotu 0
+    ret                   # Powrót
 
-#funkcja f ma obliczac ciag fibonacciego rekurencyjnie
+f:
+    mov eax, [esp+4]      # Pobierz argument funkcji f
+    cmp eax, 1            # Sprawdź, czy eax <= 1
+    jbe base_case         # Jeśli tak, przejdź do base_case
+    push eax              # Zachowaj wartość eax na stosie
+    
+    dec eax               # Oblicz n-1
+    push eax              # Umieść n-1 na stosie
+    call f                # Wywołaj f(n-1)
+    add esp, 4            # Usuń n-1 ze stosu
+    mov ebx, eax          # Zachowaj wynik f(n-1) w ebx
+    
+    mov eax, [esp+4]      # Pobierz n ze stosu ponownie
+    sub eax, 2            # Oblicz n-2
+    push eax              # Umieść n-2 na stosie
+    call f                # Wywołaj f(n-2)
+    add esp, 4            # Usuń n-2 ze stosu
+    add eax, ebx          # Dodaj wynik f(n-2) do wyniku f(n-1)
+    
+    ret                   # Powrót z wynikiem
+    
+base_case:
+    mov eax, 1            # Jeśli n <= 1, zwróć 1
+    ret                   # Powrót
 
 .data
 msg:
 .asciz "Wynik = %i \n"
-
-#f(n) = 1 dla n=1 i n=2
-#f(n-1)+f(n-2) dla n>2
-#
-#musimy wykorzystac stos w jakis sprytny sposob
